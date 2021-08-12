3667d57305b64639c54000d8b77b6675d39206e2
William Crews <crewswh@gmail.com>
2021-08-12 17:14:40 -0400

    inputs time

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,9 +1,18 @@
+var zipCode = document.getElementById('zipCode').text;
+var city = document.getElementById('city');
+
+
+
 $( "#theSubmit" ).click(function() {
-    console.log("cat");
+    localStorage.setItem("zipz", zipCode)
+
+    var zipz = localStorage.getItem("zipz")
+    
+    console.log(zipz);
 });
 
 $( "#buttOne" ).click(function() {
-    console.log("duck");
+    console.log(city);
 });
 
 $( "#buttTwo" ).click(function() {
@@ -11,5 +20,13 @@ $( "#buttTwo" ).click(function() {
 });
 
 $( "#buttThree" ).click(function() {
+    console.log("bass");
+});
+
+$( "#buttFour" ).click(function() {
+    console.log("dog");
+});
+
+$( "#buttFive" ).click(function() {
     console.log("falcon");
 });
